48fcc99d3546151a291adcc3f7fea90bd27a3b5b
wuhuikai <huikaiwu@icloud.com>
2019-09-24 10:03:32 +0800

    rm baseline methods

--- Baseline/ModPoissonBlending.m
+++
@@ -1,9 +0,0 @@
-%the code is created according to Masayuki Tanaka's code
-function [res]=ModPoissonBlending(ftrg,src,mask)
-    targetFeature = imGradFeature(ftrg);
-    sourceFeature = imGradFeature(src);
-    mask = repmat(double(mask), [1, 1, 3, 5]);
-    targetFeature = targetFeature .* (1-mask) + sourceFeature .* mask;
-    param = buildModPoissonParam(size(targetFeature));
-    res = modPoisson(targetFeature, param, 1E-8);
-end
--- Baseline/NaiveBlending.m
+++
@@ -1,4 +0,0 @@
-function [naive_img]=NaiveBlending(trg,src,mask)
-    naive_img=trg;
-    naive_img(repmat(mask,[1,1,3]))=src(repmat(mask,[1,1,3]));
-end
